472524ea7f282b5bc5d40fe674e47c7ef606cbde
calmen <calmenchia@gmail.com>
2022-11-28 18:55:13 +0800

    Designed login page

--- centre-booking/wwwroot/js/Home.jsx
+++ centre-booking/wwwroot/js/Home.jsx
@@ -1,7 +1,67 @@
 ﻿class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        // binding that allows for callbacks when the form is submitted
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    handleChange({ target }) {
+        this.setState({
+            [target.name]: target.value
+        });
+    }
+
+    handleLogin() {
+        if (this.state.username === '' && this.state.password === '') {
+            alert('Missing username or password!');
+        } else {
+            alert('OK! username: ' + this.state.username + ', Password: ' + this.state.password);
+        }
+    }
+
     render() {
         return (
-            <h1>Hello World</h1>
+            <div className="home-page">
+                <h1>Centre Booking</h1>
+                <br />
+                <form>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Username"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <button
+                        id="loginBtn"
+                        onClick={this.handleLogin}
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <h1>Menu</h1>
         );
     }
 }
